461b09a30dd0f86342cce525a9662bf3e659d17e
h4x4d <sidenkoleg@gmail.com>
2024-11-19 14:57:50 +0300

    filters implemented

--- hotel/internal/services/get_hotels.go
+++ hotel/internal/services/get_hotels.go
@@ -6,6 +6,7 @@ import (
 	"github.com/h4x4d/go_hsse_hotels/hotel/internal/models"
 	"github.com/jackc/pgx/v5"
 	"os"
+	"strings"
 )
 
 func GetHotels(city *string, hotel_class *int64, name *string, tag *string) ([]*models.Hotel, error) {
@@ -17,8 +18,26 @@ func GetHotels(city *string, hotel_class *int64, name *string, tag *string) ([]*
 	}
 	defer conn.Close(context.Background())
 
-	// getting query for hotels
-	rowsHotels, errQueryHotels := conn.Query(context.Background(), "SELECT * FROM hotels")
+	query := `SELECT * FROM hotels`
+	clauses := []string{}
+	args := []interface{}{}
+	if city != nil {
+		clauses = append(clauses, fmt.Sprintf("city = $%d", len(clauses)+1))
+		args = append(args, *city)
+	}
+	if hotel_class != nil {
+		clauses = append(clauses, fmt.Sprintf("hotel_class = $%d", len(clauses)+1))
+		args = append(args, *hotel_class)
+	}
+	if name != nil {
+		clauses = append(clauses, fmt.Sprintf("NAME LIKE $%d", len(clauses)+1))
+		args = append(args, "%"+*name+"%")
+	}
+	if len(clauses) > 0 {
+		query += " WHERE " + strings.Join(clauses, " AND ")
+	}
+
+	rowsHotels, errQueryHotels := conn.Query(context.Background(), query, args...)
 	if errQueryHotels != nil {
 		return nil, err
 	}
@@ -56,6 +75,7 @@ func GetHotels(city *string, hotel_class *int64, name *string, tag *string) ([]*
 			currRoom.Cost = new(int64)
 			currRoom.HotelID = new(int64)
 			currRoom.PersonCount = new(int64)
+			currRoom.Tags = make([]*models.Tag, 0)
 
 			// scanning room
 			errRoom := rowsRooms.Scan(&currRoom.ID, currRoom.HotelID, currRoom.Cost, currRoom.PersonCount)
@@ -67,14 +87,18 @@ func GetHotels(city *string, hotel_class *int64, name *string, tag *string) ([]*
 		rowsRooms.Close()
 	}
 	// getting tags information
-	for _, currHotel := range result {
-		for _, currRoom := range currHotel.Rooms {
+	for hotelIndex := 0; hotelIndex < len(result); {
+		index := 0
+		currHotel := result[hotelIndex]
+		for index < len(currHotel.Rooms) {
+			currRoom := currHotel.Rooms[index]
 			// getting query for tags
 			rowsTags, errQueryTags := conn.Query(context.Background(),
 				"SELECT * FROM tags WHERE room_id = $1", currRoom.ID)
 			if errQueryTags != nil {
 				return nil, err
 			}
+			suit := tag == nil
 			// reading rooms
 			for rowsTags.Next() {
 				currTag := new(models.Tag)
@@ -86,9 +110,22 @@ func GetHotels(city *string, hotel_class *int64, name *string, tag *string) ([]*
 				if errTag != nil {
 					return nil, err
 				}
+				if tag != nil && *currTag.Name == *tag {
+					suit = true
+				}
 				currRoom.Tags = append(currRoom.Tags, currTag)
 			}
 			rowsTags.Close()
+			if !suit {
+				currHotel.Rooms = append(currHotel.Rooms[:index], currHotel.Rooms[index+1:]...)
+			} else {
+				index++
+			}
+		}
+		if index == 0 {
+			result = append(result[:hotelIndex], result[hotelIndex+1:]...)
+		} else {
+			hotelIndex++
 		}
 	}
 	return result, nil
