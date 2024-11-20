df862846cd90fc048ba09ed4f3bf6f325f7c34fc
h4x4d <sidenkoleg@gmail.com>
2024-11-20 10:35:56 +0300

    fix grpc handler

--- hotel/internal/grpc/handlers/get_room.go
+++ hotel/internal/grpc/handlers/get_room.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	gen "github.com/h4x4d/go_hsse_hotels/hotel/internal/grpc/gen"
 	"github.com/h4x4d/go_hsse_hotels/hotel/internal/models"
+	"github.com/h4x4d/go_hsse_hotels/hotel/internal/services"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
@@ -18,7 +19,7 @@ type HotelServer interface {
 	GetRoom(
 		ctx context.Context,
 		id int64,
-	) (models.Room, error)
+	) (*models.Room, error)
 }
 
 func Register(gRPCServer *grpc.Server, hotel HotelServer) {
@@ -27,7 +28,7 @@ func Register(gRPCServer *grpc.Server, hotel HotelServer) {
 
 func (serverApi *GRPCServer) GetRoom(
 	ctx context.Context, in *gen.RoomRequest) (*gen.RoomResponse, error) {
-	room, err := serverApi.hotelServer.GetRoom(ctx, in.Id)
+	room, err := services.GetRoom(int(in.Id))
 	if err != nil {
 		return nil, status.Errorf(codes.Internal, "internal error: %v", err)
 	}
