e3c36f8e4c064236d34fe3016b40ee8184139737
h4x4d <sidenkoleg@gmail.com>
2024-11-26 15:06:39 +0300

    update hotel change

--- hotel/internal/restapi/handlers/update_hotel.go
+++ hotel/internal/restapi/handlers/update_hotel.go
@@ -1,13 +1,27 @@
 package handlers
 
 import (
+	"fmt"
 	"github.com/go-openapi/runtime/middleware"
+	"github.com/h4x4d/go_hsse_hotels/hotel/internal/models"
 	"github.com/h4x4d/go_hsse_hotels/hotel/internal/restapi/operations/hotel"
 	"github.com/h4x4d/go_hsse_hotels/hotel/internal/utils"
 )
 
 func (handler *Handler) UpdateHotelHandler(params hotel.UpdateHotelParams, _ interface{}) (responder middleware.Responder) {
 	defer utils.CatchPanic(&responder)
+
+	exists, errGet := handler.Database.Exists(params.HotelID)
+	if errGet != nil {
+		return utils.HandleInternalError(errGet)
+	}
+	if exists == false {
+		code := int64(hotel.UpdateHotelNotFoundCode)
+		return &hotel.UpdateHotelNotFound{Payload: &models.Error{
+			ErrorMessage:    fmt.Sprintf("no hotel with id %d", params.HotelID),
+			ErrorStatusCode: &code,
+		}}
+	}
 
 	newHotel := params.Object
 	updated, errUpdate := handler.Database.Update(params.HotelID, newHotel)
